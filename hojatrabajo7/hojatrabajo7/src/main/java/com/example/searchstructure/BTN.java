ea374f54c93eb1220b46e3da1d6bfe7556774cf4
Adrian Arimany <176186615+adrianArimany@users.noreply.github.com>
2025-03-31 10:47:02 -0600

    added java doc.

--- hojatrabajo7/hojatrabajo7/src/main/java/com/example/searchstructure/BTN.java
+++ hojatrabajo7/hojatrabajo7/src/main/java/com/example/searchstructure/BTN.java
@@ -18,39 +18,95 @@ public final class BTN<K extends Comparable<K>, V> {
         setIsRightChild(isrightchild);
     }   
     
+    /**
+     * @return the key for this node
+     */
     public K get_key() {
         return _key;
     }
+    /**
+     * Set the key for this node.
+     * 
+     * @param _key
+     *            the key for this node
+     */
     public void set_key(K _key) {
         this._key = _key;
     }
+    /**
+     * @return the value associated with this node
+     */
     public V get_value() {
         return _value;
     }
+    /**
+     * Set the value associated with this node.
+     * 
+     * @param _value
+     *            the value to associate with this node
+     */
     public void set_value(V _value) {
         this._value = _value;
     }
+    /**
+     * @return the left child node of this node, or null if there is no left child
+     */
+
     public BTN<K, V> get_leftChild() {
         return _leftChild;
     }
+    /**
+     * Set the left child of this node.
+     * 
+     * @param _leftChild
+     *            the left child of this node
+     */
     public void set_leftChild(BTN<K, V> _leftChild) {
         this._leftChild = _leftChild;
     }
+    /**
+     * @return the right child node of this node, or null if there is no right child
+     */
     public BTN<K, V> get_rightChild() {
         return _rightChild;
     }
+    /**
+     * Set the right child of this node.
+     * 
+     * @param _rightChild
+     *            the right child of this node
+     */
     public void set_rightChild(BTN<K, V> _rightChild) {
         this._rightChild = _rightChild;
     }
+    /**
+     * @return the parent node of this node, or null if this node is the root
+     */
     public BTN<K, V> get_parent() {
         return _parent;
     }
+    /**
+     * Set the parent of this node.
+     * 
+     * @param _parent
+     *            the parent of this node, or null if this node is the root
+     */
     public void set_parent(BTN<K, V> _parent) {
         this._parent = _parent;
     }
+    /**
+     * @return true if this node is the right child of its parent, false otherwise
+     */
     public boolean isRightChild() {
         return isRightChild;
     }
+    /**
+     * Sets whether this node is the right child of its parent.
+     *
+     * @param isRightChild
+     *            true if this node is the right child, false otherwise
+     */
+
     public void setIsRightChild(boolean isRightChild) {
         this.isRightChild = isRightChild;
     }
--- hojatrabajo7/hojatrabajo7/src/main/java/com/example/searchstructure/ListElements.java
+++ hojatrabajo7/hojatrabajo7/src/main/java/com/example/searchstructure/ListElements.java
@@ -6,6 +6,14 @@ public class ListElements<K extends Comparable<K>, V> implements ITraversal<K , 
 
     public ArrayList<V> elementos = new ArrayList<>();
 
+    /**
+     * Agrega el valor de un nodo a una lista.
+     *
+     * <p>Este m todo se utiliza como parte del patr n de dise o
+     * <i>Visitor</i> para recorrer un  rbol binario de b squeda y
+     * extraer sus valores.</p>
+     *
+     */
     @Override
     public void check(BTN<K, V> actualNode) {
         elementos.add(actualNode.get_value());
