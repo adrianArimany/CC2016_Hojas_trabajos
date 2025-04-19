8397deaae5475465266a9e9615fdaa5506a52e4c
RodrigoAar <ajm22279@uvg.edu.gt>
2025-04-19 13:19:05 -0600

    Update main.py
    Se agregó que pudieran haber una o más enfermeras registrando pacientes. Además que a partir de la evaluación ahora se puede consultar con un doctor o ser operado.

--- hojatrabajo8/SimpySimulation/main.py
+++ hojatrabajo8/SimpySimulation/main.py
@@ -4,13 +4,16 @@ import simpy
 
 random.seed(12)
 
-def simulation(env, nombre, RayosX, Laboratorio):
+def simulation(env, nombre,Enfermeras, RayosX, Laboratorio, Doctores, Operaciones):
     print(f"El paciente {nombre} ha ingresado a la sala de espera en el tiempo {env.now}")
     arrival_time = env.now
     prioridad = random.randint(1,5) #Prioridad del paciente
-    yield env.timeout(10) #Se lleva a cabo el registro
-    proceso = random.randint(1,2) #Proceso que se llevará a cabo 1)Rayos X 2)Examen de laboratorio
-    print(f"Se ha registrado al paciente {nombre}, con prioridad {prioridad} en el tiempo {env.now}")
+    with Enfermeras.request() as req:
+        yield req
+        print(f"El paciente {nombre} estpa siendo atendido por una enfermera")
+        yield env.timeout(10)
+        print(f"El paciente {nombre}, prioridad: {prioridad}, ha sido registrado en el tiempo {env.now}")
+        proceso = random.randint(1,4)
     if proceso == 1:
         with RayosX.request(priority = prioridad) as req:
             print(f"El paciente {nombre} espera los rayos X")
@@ -18,13 +21,28 @@ def simulation(env, nombre, RayosX, Laboratorio):
             wating_time = env.now - arrival_time
             print(f"El paciente {nombre} ha ingresado al tomógrafo en el tiempo {env.now}")
             yield env.timeout(10)
-    else:
+    elif proceso == 2:
         with Laboratorio.request(priority = prioridad) as req:
             print(f"El paciente {nombre} espera el Laboratorio")
             yield req
             wating_time = env.now - arrival_time
             print(f"El paciente {nombre} ha ingresado al laboratorio en el tiempo {env.now}")
             yield env.timeout(30)
+    elif proceso == 3:
+        with Doctores.request(priority = prioridad) as req:
+            print(f"El paciente {nombre} espera ser atendido por un doctor")
+            yield req
+            wating_time = env.now - arrival_time
+            print(f"El paciente {nombre} ha ingresado a consulta con un doctor en el tiempo {env.now}")
+            yield env.timeout(30)
+    elif proceso == 4:
+        with Operaciones.request(priority = prioridad) as req:
+            print(f"El paciente {nombre} espera para ser operado")
+            yield req
+            wating_time = env.now - arrival_time
+            print(f"El paciente {nombre} ha ingresado a la sala de operaciones en el tiempo {env.now}")
+            op_time = random.randint(120,240)
+            yield env.timeout(op_time)
 
     print(f"El paciente {nombre} (prioridad {prioridad}) ha salido en el tiempo {env.now}, tuvo que esperar {wating_time} para ser atendido")
 
