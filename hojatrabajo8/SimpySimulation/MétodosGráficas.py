6493f793543edd2cbb548439316ae63517fe9883
RodrigoAar <ajm22279@uvg.edu.gt>
2025-04-20 12:38:05 -0600

    Comentar el código

--- hojatrabajo8/SimpySimulation/MétodosGráficas.py
+++ hojatrabajo8/SimpySimulation/MétodosGráficas.py
@@ -7,14 +7,17 @@ import pandas as pd
 
 def run_experiments(recurso_objetivo, capacidades, num_pacientes, interval):
     resultados_globales = []
+    #Realizar variando la capacidad de un recurso en específico
     for cap in capacidades:
+        #Estas con las capacidades fijas del hospital
         env = simpy.Environment()
-        Enfermeras = simpy.Resource(env, capacity=2)
-        RayosX = simpy.PriorityResource(env, capacity=2)
-        Laboratorio = simpy.PriorityResource(env, capacity=5)
-        Operaciones = simpy.PriorityResource(env, capacity=2)
-        Doctores = simpy.PriorityResource(env, capacity=3)
+        Enfermeras = simpy.Resource(env, capacity=2) #2 enfermeras registrando
+        RayosX = simpy.PriorityResource(env, capacity=2) #2 tomógrafos
+        Laboratorio = simpy.PriorityResource(env, capacity=5) #5 técnicos de laboratorio
+        Operaciones = simpy.PriorityResource(env, capacity=2) #2 equipos de operaciones (cada equipo con 3 doctores y 2 enfermeras)
+        Doctores = simpy.PriorityResource(env, capacity=3) #3 doctores en consulta
 
+        #Dependiendo que recurso es el que se desee variar en cierto rango irá cambiando la capacidad
         if recurso_objetivo == 'Doctores':
             Doctores = simpy.PriorityResource(env, capacity=cap)
         elif recurso_objetivo == 'Operaciones':
@@ -26,12 +29,14 @@ def run_experiments(recurso_objetivo, capacidades, num_pacientes, interval):
         elif recurso_objetivo == 'Enfermeras':
             Enfermeras = simpy.Resource(env, capacity=cap)
 
+        #Realizar la simulación
         resultados = []
         env.process(
             generate_simulation(env, Enfermeras, RayosX, Laboratorio, Doctores, Operaciones, num_pacientes, interval,
                                 resultados))
         env.run()
 
+        #Guardar resultados de todos los pacientes
         for r in resultados:
             if r["recurso"] == recurso_objetivo:
                 r["capacidad"] = cap
@@ -43,6 +48,7 @@ def graficar_resultados(resultados, recurso_objetivo):
     df = pd.DataFrame(resultados)
     df_filtrado = df[df["recurso"] == recurso_objetivo]
 
+    #Realizar la gráfica para cada una de las 5 prioridades con el tiempo promedio de espera. Se utilizan los reultados del método anterior
     plt.figure(figsize=(10, 6))
     for prioridad in sorted(df_filtrado["prioridad"].unique()):
         subset = df_filtrado[df_filtrado["prioridad"] == prioridad]
--- hojatrabajo8/SimpySimulation/main.py
+++ hojatrabajo8/SimpySimulation/main.py
@@ -5,30 +5,30 @@ import simpy
 random.seed(11)
 
 def simulation(env, nombre,Enfermeras, RayosX, Laboratorio, Doctores, Operaciones, resultados):
-    print(f"El paciente {nombre} ha ingresado a la sala de espera en el tiempo {env.now}")
+    print(f"El paciente {nombre} ha ingresado a la sala de espera en el tiempo {env.now}") #Informar a que hora ingresó el usuario
     arrival_time = env.now
     prioridad = random.randint(1,5) #Prioridad del paciente
     with Enfermeras.request() as req:
-        yield req
+        yield req #Esperar a ser atendido por una enfermera
         print(f"El paciente {nombre} está siendo registrado por una enfermera")
-        yield env.timeout(10)
-        reg_time = env.now - arrival_time
+        yield env.timeout(10) #Llevar a cabo registro
+        reg_time = env.now - arrival_time #Registrar a que "hora" ya se encontraba registrado el paciente
         print(f"El paciente {nombre}, prioridad: {prioridad}, ha sido registrado en el tiempo {env.now}")
-        proceso = random.randint(1,4)
+        proceso = random.randint(1,4) #Simular el procedimiento desginado al paciente (1 Rayos X, (2 Examenes de laboratorio, (3 Ser atendido por un doctor, (4 Operación
         resultados.append({
             "prioridad": prioridad,
-            "waiting_time": reg_time,  # usamos el tiempo de registro
+            "waiting_time": reg_time,  # usamos el tiempo de registro (guardar cuanto tiempo se tardaron en registrar al paciente)
             "recurso": "Enfermeras"
         })
     if proceso == 1:
         with RayosX.request(priority = prioridad) as req:
-            print(f"El paciente {nombre} espera los rayos X")
-            yield req
-            wating_time = env.now - arrival_time
+            print(f"El paciente {nombre} espera los rayos X") #Indicar que el paciente se encuentra en la sala de espera, designado a RayosX
+            yield req #Esperar espacio en un tomógrafo
+            wating_time = env.now - arrival_time #Registrar cuanto tiempo tardó en llegar hasta entrar al tomógrafo
             print(f"El paciente {nombre} ha ingresado al tomógrafo en el tiempo {env.now}")
-            yield env.timeout(10)
+            yield env.timeout(10) #Simular radiografía
             proceso_nombre = "RayosX"
-            resultados.append({
+            resultados.append({ #Guardar resultados de tiempo de espera
                 "prioridad": prioridad,
                 "waiting_time": wating_time,
                 "recurso": proceso_nombre
@@ -36,13 +36,13 @@ def simulation(env, nombre,Enfermeras, RayosX, Laboratorio, Doctores, Operacione
 
     elif proceso == 2:
         with Laboratorio.request(priority = prioridad) as req:
-            print(f"El paciente {nombre} espera el Laboratorio")
-            yield req
-            wating_time = env.now - arrival_time
+            print(f"El paciente {nombre} espera el Laboratorio") #Indicar que el paciente se encuentra en la sala de espera, designado a Laboratorio
+            yield req #Esperar espacio en el laboratorio
+            wating_time = env.now - arrival_time #Registrar cuanto tiempo tardó en llegar hasta que se le realizaran los examenes
             print(f"El paciente {nombre} ha ingresado al laboratorio en el tiempo {env.now}")
-            yield env.timeout(30)
+            yield env.timeout(30) #Simular examenes de laboratorio
             proceso_nombre = "Laboratorio"
-            resultados.append({
+            resultados.append({ #Guardar resultados de tiempo de espera
                 "prioridad": prioridad,
                 "waiting_time": wating_time,
                 "recurso": proceso_nombre
@@ -50,13 +50,13 @@ def simulation(env, nombre,Enfermeras, RayosX, Laboratorio, Doctores, Operacione
 
     elif proceso == 3:
         with Doctores.request(priority = prioridad) as req:
-            print(f"El paciente {nombre} espera ser atendido por un doctor")
-            yield req
-            wating_time = env.now - arrival_time
+            print(f"El paciente {nombre} espera ser atendido por un doctor") #Indicar que el paciente se encuentra en la sala de espera, designado a ser atendido por un doctor
+            yield req #Esperar disponibilidad de un doctor
+            wating_time = env.now - arrival_time #Registrar cuanto tiempo tardó en llegar hasta que se fuera atendido por un doctor
             print(f"El paciente {nombre} ha ingresado a consulta con un doctor en el tiempo {env.now}")
-            yield env.timeout(30)
+            yield env.timeout(30) #Simular consulta con un doctor
             proceso_nombre = "Doctores"
-            resultados.append({
+            resultados.append({ #Guardar resultados de tiempo de espera
                 "prioridad": prioridad,
                 "waiting_time": wating_time,
                 "recurso": proceso_nombre
@@ -64,14 +64,14 @@ def simulation(env, nombre,Enfermeras, RayosX, Laboratorio, Doctores, Operacione
 
     elif proceso == 4:
         with Operaciones.request(priority = prioridad) as req:
-            print(f"El paciente {nombre} espera para ser operado")
-            yield req
-            wating_time = env.now - arrival_time
+            print(f"El paciente {nombre} espera para ser operado") #Indicar que el paciente se encuentra en la sala de espera, designado a ser operado
+            yield req #Esperar disponibilidad de quirófano
+            wating_time = env.now - arrival_time #Registrar cuanto tiempo tardó en llegar hasta que se fuera atendido para operación
             print(f"El paciente {nombre} ha ingresado a la sala de operaciones en el tiempo {env.now}")
             op_time = random.randint(120,240)
-            yield env.timeout(op_time)
+            yield env.timeout(op_time) #Simular tiempo de operación, generalmente una operación de emergencia dura entre 2 y 4 horas
             proceso_nombre = "Operaciones"
-            resultados.append({
+            resultados.append({ #Guardar resultados de tiempo de espera
                 "prioridad": prioridad,
                 "waiting_time": wating_time,
                 "recurso": proceso_nombre
@@ -80,6 +80,7 @@ def simulation(env, nombre,Enfermeras, RayosX, Laboratorio, Doctores, Operacione
     print(f"El paciente {nombre} (prioridad {prioridad}) ha salido en el tiempo {env.now}, tuvo que esperar {reg_time} para ser registrado y {wating_time} para ser atendido según su condición")
 
 def generate_simulation(env, Enfermeras, RayosX, Laboratorio, Doctores, Operaciones, num_pacientes, interval, resultados):
+    #Generar la simulación para n cantidad de pacientes
     for i in range(num_pacientes):
         env.process(simulation(env, str(i), Enfermeras, RayosX, Laboratorio, Doctores, Operaciones, resultados))
         yield env.timeout(random.expovariate(1/interval))
